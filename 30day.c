3306d80ef15ce4581dd20d7a93ac16d831af318d
aryandd657 <aryandhiman3552@gmail.com>
2026-03-02 22:23:45 +0530

    Add files via upload

---
+++ 30day.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int coeff;
+    int exp;
+    struct Node* next;
+};
+
+// Function to create new node
+struct Node* createNode(int c, int e) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->coeff = c;
+    newNode->exp = e;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Insert at end
+struct Node* insert(struct Node* head, int c, int e) {
+    struct Node* temp = createNode(c, e);
+
+    if (head == NULL)
+        return temp;
+
+    struct Node* p = head;
+    while (p->next != NULL)
+        p = p->next;
+
+    p->next = temp;
+    return head;
+}
+
+// Display Polynomial
+void display(struct Node* head) {
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        if (temp->exp == 0)
+            printf("%d", temp->coeff);
+        else if (temp->exp == 1)
+            printf("%dx", temp->coeff);
+        else
+            printf("%dx^%d", temp->coeff, temp->exp);
+
+        if (temp->next != NULL)
+            printf(" + ");
+
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, c, e;
+    struct Node* head = NULL;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter coefficient and exponent: ");
+        scanf("%d %d", &c, &e);
+        head = insert(head, c, e);
+    }
+
+    printf("\nPolynomial: ");
+    display(head);
+
+    return 0;
+}
