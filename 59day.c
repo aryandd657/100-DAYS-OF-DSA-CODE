9ff5e1f6eefd2de02747ccd41c2947615b144e1c
aryandd657 <aryandhiman3552@gmail.com>
2026-04-02 15:36:01 +0530

    Add files via upload

---
+++ 59day.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Tree Node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* createNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Find index in inorder
+int findIndex(int inorder[], int start, int end, int val) {
+    for (int i = start; i <= end; i++) {
+        if (inorder[i] == val)
+            return i;
+    }
+    return -1;
+}
+
+// Build Tree
+struct Node* buildTree(int inorder[], int postorder[], int inStart, int inEnd, int* postIndex) {
+    if (inStart > inEnd)
+        return NULL;
+
+    int rootVal = postorder[*postIndex];
+    (*postIndex)--;
+
+    struct Node* root = createNode(rootVal);
+
+    if (inStart == inEnd)
+        return root;
+
+    int inIndex = findIndex(inorder, inStart, inEnd, rootVal);
+
+    // IMPORTANT: build right first
+    root->right = buildTree(inorder, postorder, inIndex + 1, inEnd, postIndex);
+    root->left  = buildTree(inorder, postorder, inStart, inIndex - 1, postIndex);
+
+    return root;
+}
+
+// Preorder Traversal (Root → Left → Right)
+void preorder(struct Node* root) {
+    if (root == NULL)
+        return;
+
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+// Main
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int* inorder = (int*)malloc(n * sizeof(int));
+    int* postorder = (int*)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &inorder[i]);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &postorder[i]);
+
+    int postIndex = n - 1;
+
+    struct Node* root = buildTree(inorder, postorder, 0, n - 1, &postIndex);
+
+    // Print preorder
+    preorder(root);
+
+    free(inorder);
+    free(postorder);
+
+    return 0;
+}
